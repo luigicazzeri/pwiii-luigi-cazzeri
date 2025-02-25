9c199c403f72fea722ce9bcb5b01fe64f2659ff4
luigicazzeri <luigi.locatelli@etec.sp.gov.br>
2025-02-24 22:24:05 -0300

    Aula sobre banco de dados

--- aula01/routes/web.php
+++ aula01/routes/web.php
@@ -9,3 +9,20 @@ Route::get('/welcome', function () {
 Route::get('/comeco', function () {
     return view('comeco');
 });
+
+Route::get('/users/{param}/{paramii}', function ($param, $paramii) {
+    return $param. '-' . $paramii;
+});
+
+Route::prefix('usuarios')->group(function(){
+    route::get('/edit', function() {
+        return 'edit';
+    });
+    route::get('/delete', function() {
+        return 'delete';
+    });
+});
+
+Route::get('/dados-de-usuarios/{user}', function(\App\Models\User $user){
+    return $user;
+});
